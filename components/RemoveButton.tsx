1e596132837d573939408d088a74ddcc3b5d24f2
Your Name <you@example.com>
2026-03-04 14:21:58 -0600

    Fix RemoveButton playerId type

--- components/RemoveButton.tsx
+++ components/RemoveButton.tsx
@@ -1,19 +1,29 @@
 "use client";
 
 import { useTransition } from "react";
-import { toggleWatchlistPlayer } from "@/app/actions/watchlist";
+import { toggleWatchlistPlayer } from "@/app/actions/player-watchlist";
 
-export function RemoveButton({ playerId }: { playerId: number }) {
+type Props = {
+  playerId: number;
+};
+
+export default function RemoveButton({ playerId }: Props) {
   const [isPending, startTransition] = useTransition();
+
+  function handleClick() {
+    startTransition(() => {
+      void toggleWatchlistPlayer(String(playerId));
+    });
+  }
 
   return (
     <button
       type="button"
-      onClick={() => startTransition(() => toggleWatchlistPlayer(playerId))}
+      onClick={handleClick}
       disabled={isPending}
       className="text-sm text-red-600 hover:underline disabled:opacity-50"
     >
-      Remove
+      {isPending ? "Removing..." : "Remove"}
     </button>
   );
 }
